0beda9f8f136589028efe917934d0c835c947d88
jinseok PARK <distoma@gmail.com>
2023-02-03 15:45:40 +0900

    add pub.dev camera deep ar

--- lib/cameraWithMaskFiltersScreen.dart
+++ lib/cameraWithMaskFiltersScreen.dart
@@ -1,4 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:camera_deep_ar/camera_deep_ar.dart';
+
+enum Masks {
+  none,
+  aviators,
+  bigmouth,
+  dalmatian,
+  bcgSeg,
+  look2,
+  fatify,
+  flowers,
+  grumpycat,
+  koala,
+  lion,
+  mudMask,
+  obama,
+  pug,
+  slash,
+  sleepingmask,
+  smallface,
+  teddycigar,
+  tripleface,
+  twistedFace,
+}
+
+enum Effects {
+  none,
+  fire,
+  heart,
+  blizzard,
+  rain,
+}
+
+enum Filters {
+  none,
+  tv80,
+  drawingmanga,
+  sepia,
+  bleachbypass,
+  realvhs,
+  filmcolorperfection,
+}
+
 
 class CameraWithMaskFiltersScreen extends StatefulWidget {
   const CameraWithMaskFiltersScreen({Key? key}) : super(key: key);
@@ -10,17 +53,23 @@ class CameraWithMaskFiltersScreen extends StatefulWidget {
 
 class _CameraWithMaskFiltersScreenState
     extends State<CameraWithMaskFiltersScreen> {
+  late CameraDeepArController cameraDeepArController;
+  String platformVersion = "Unknown";
+  int currentPage = 0;
+  final vp = PageController(viewportFraction: .24);
+  Effects currentEffects = Effects.none;
+  Filters currentFilters = Filters.none;
+  Masks currentMask = Masks.none;
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text(
-          "camera with mask filters",
-          style: TextStyle(
-            fontSize: 25,
-            fontWeight: FontWeight.bold,
-            color: Colors.deepPurpleAccent,
-          ),
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: Stack(
+          children: [
+            // deep AR Camera
+          ],
         ),
       ),
     );
